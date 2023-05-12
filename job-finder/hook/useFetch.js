b745107036d4aeeb01b3fa6e9174800ddb944c34
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-05-12 14:01:13 +0700

    complete useFetch func component

--- job-finder/hook/useFetch.js
+++ job-finder/hook/useFetch.js
@@ -1,8 +1,47 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-const useFetch = () => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+          'X-RapidAPI-Key': 'a8782a29ebmsh71e6f931300a16ap189d70jsnb856687d4a7f',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true)
+
+        try {
+            const response = await axios.request(options)
+            setData(response.data.data)
+            setIsLoading(false)
+        } catch (error) {
+            setError(error)
+            alert("There is an error")
+            console.log("GGGG error in fetchData func: ", error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true)
+        fetchData()
+    }
+
+    return { data, isLoading, error, refetch}
 }
+
+export default useFetch
